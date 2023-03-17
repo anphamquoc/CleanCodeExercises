7f748d43f9c82f075261987aa8161ff0d44d33db
Pham Quoc An <an.pham.quoc@preciofishbone.se>
2023-03-17 17:25:09 +0700

    Create new functions and type for refactoring PaymentService

---
+++ TrickBookStoreExercises/Models/RemainingSubscriptionType.cs
@@ -0,0 +1,7 @@
+namespace TrickBookStoreExercises.Models;
+
+public enum RemainingSubscriptionType
+{
+    Premium = 0,
+    Paid = -1
+}
--- TrickBookStoreExercises/Services/Payment/PaymentService.cs
+++ TrickBookStoreExercises/Services/Payment/PaymentService.cs
@@ -33,42 +33,9 @@ internal class PaymentService : IPaymentService
 
         foreach (var subscription in foundCustomer.Subscriptions)
         {
-            var subscriptionId = subscription.Id;
-            var subscriptionType = subscription.SubscriptionType;
-
             totalPaymentAmount += subscription.PriceDetails["FixPrice"];
 
-            switch (subscriptionType)
-            {
-                case SubscriptionTypes.Free:
-                case SubscriptionTypes.Paid:
-                    oldBookCharge = Math.Min(oldBookCharge, subscription.PriceDetails["OldBookCharge"]);
-
-                    // in case of paid account
-                    if (subscriptionType == SubscriptionTypes.Paid)
-                        remainingSubscriptions.Add(-1, 3);
-
-                    continue;
-
-                // in case of premium account
-                case SubscriptionTypes.Premium:
-                    oldBookCharge = 0;
-                    remainingSubscriptions.Add(0, 3);
-
-                    continue;
-            }
-
-            var bookCategoryId = subscription.BookCategoryId ?? -1;
-
-            if (bookCategoryId == -1)
-            {
-                Console.WriteLine(
-                    $"This book category subscription with subscription id {subscriptionId} is not valid.");
-
-                continue;
-            }
-
-            remainingSubscriptions.Add(bookCategoryId, 3);
+            UpdateRemainingSubscriptions(subscription, ref remainingSubscriptions, ref oldBookCharge);
         }
 
         foreach (var purchaseTransaction in foundPurchaseTransactions)
@@ -85,31 +52,7 @@ internal class PaymentService : IPaymentService
                 continue;
             }
 
-            // Check and use remaining selected category subscription
-            if (CheckSubscription(remainingSubscriptions, bookCategoryId))
-            {
-                remainingSubscriptions[bookCategoryId]--;
-                totalPaymentAmount += 0.85 * bookPrice;
-                continue;
-            }
-
-            // Check and use remaining premium subscription
-            if (CheckSubscription(remainingSubscriptions, 0))
-            {
-                remainingSubscriptions[0]--;
-                totalPaymentAmount += 0.85 * bookPrice;
-                continue;
-            }
-
-            // Check and use remaining paid subscription
-            if (CheckSubscription(remainingSubscriptions, -1))
-            {
-                remainingSubscriptions[-1]--;
-                totalPaymentAmount += 0.95 * bookPrice;
-                continue;
-            }
-
-            totalPaymentAmount += bookPrice;
+            totalPaymentAmount += GetNewBookPrice(purchaseTransaction.Book, ref remainingSubscriptions);
         }
 
         return Math.Round(totalPaymentAmount, 2);
@@ -121,5 +64,66 @@ internal class PaymentService : IPaymentService
             return value != 0;
 
         return false;
+    }
+
+    private static double GetNewBookPrice(Book book, ref Dictionary<int, int> remainingSubscriptions)
+    {
+        var discount = 1.0;
+        var categoryIdToCheck = book.CategoryId;
+
+        if (CheckSubscription(remainingSubscriptions, categoryIdToCheck))
+        {
+            remainingSubscriptions[categoryIdToCheck]--;
+            discount = 0.85;
+        }
+        // Checking for premium subscription
+        else if (CheckSubscription(remainingSubscriptions, (int)RemainingSubscriptionType.Premium))
+        {
+            remainingSubscriptions[(int)RemainingSubscriptionType.Premium]--;
+            discount = 0.85;
+        }
+        // Checking for paid subscription
+        else if (CheckSubscription(remainingSubscriptions, (int)RemainingSubscriptionType.Paid))
+        {
+            remainingSubscriptions[(int)RemainingSubscriptionType.Paid]--;
+            discount = 0.95;
+        }
+
+        return Math.Round(discount * book.Price, 2);
+    }
+
+    private static void UpdateRemainingSubscriptions(Subscription subscription,
+        ref Dictionary<int, int> remainingSubscriptions, ref double oldBookCharge)
+    {
+        var subscriptionId = subscription.Id;
+        var subscriptionType = subscription.SubscriptionType;
+
+        switch (subscriptionType)
+        {
+            case SubscriptionTypes.Free:
+            case SubscriptionTypes.Paid:
+                oldBookCharge = Math.Min(oldBookCharge, subscription.PriceDetails["OldBookCharge"]);
+
+                // in case of paid account
+                if (subscriptionType == SubscriptionTypes.Paid)
+                    remainingSubscriptions.Add((int)RemainingSubscriptionType.Paid, 3);
+                break;
+
+            // in case of premium account
+            case SubscriptionTypes.Premium:
+                oldBookCharge = 0;
+                remainingSubscriptions.Add((int)RemainingSubscriptionType.Premium, 3);
+                break;
+            case SubscriptionTypes.CategoryAddicted:
+            default:
+                var bookCategoryId = subscription.BookCategoryId ?? -1;
+
+                if (bookCategoryId == -1)
+                    Console.WriteLine(
+                        $"This book category subscription with subscription id {subscriptionId} is not valid.");
+
+                remainingSubscriptions.Add(bookCategoryId, 3);
+                break;
+        }
     }
 }
