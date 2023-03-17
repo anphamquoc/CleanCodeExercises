ac785a0fee27cac7be659692565da1674c9d2c78
Pham Quoc An <an.pham.quoc@preciofishbone.se>
2023-03-17 09:50:16 +0700

    Added Dependency Injection and test exercises

---
+++ TrickBookStoreExercises/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TrickyBookStore.Services.Books;
+using TrickyBookStore.Services.Customers;
+using TrickyBookStore.Services.Payment;
+using TrickyBookStore.Services.PurchaseTransactions;
+using TrickyBookStore.Services.Subscriptions;
+
+var services = new ServiceCollection();
+
+services.AddScoped<ICustomerService, CustomerService>();
+services.AddScoped<IBookService, BookService>();
+services.AddScoped<IPaymentService, PaymentService>();
+services.AddScoped<IPurchaseTransactionService, PurchaseTransactionService>();
+services.AddScoped<ISubscriptionService, SubscriptionService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var paymentService = serviceProvider.GetService<IPaymentService>();
+
+var paymentAmount = paymentService.GetPaymentAmount(6, 2, 2018);
+
+Console.WriteLine(paymentAmount);
